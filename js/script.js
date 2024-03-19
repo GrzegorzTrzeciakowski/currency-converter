3c48bc812452e0d97fd93e66c4825ffbb37532c5
Grzegorz Trzeciakowski <gtrzeciakowski@gmail.com>
2024-03-19 17:14:58 +0100

    Added functions

--- js/script.js
+++ js/script.js
@@ -4,18 +4,30 @@ const currencyElement = document.querySelector(".js-currency");
 const resultElement = document.querySelector(".js-result");
 const clearButton = document.querySelector(".js-clear");
 
+function calculateAmount(amount, rate) {
+    return amount * rate;
+}
+
+function updateResult(resultElement, result) {
+    resultElement.innerText = result.toFixed(2);
+}
+
+function clearForm(amountElement, resultElement) {
+    amountElement.value = "";
+    resultElement.innerText = "N/A";
+}
+
 formElement.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    const amountInPLN = amountInPLNElement.value;
-    const currency = currencyElement.value;
+    const amountInPLN = parseFloat(amountInPLNElement.value);
+    const currency = parseFloat(currencyElement.value);
 
-    const result = amountInPLN * currency;
-
-    resultElement.innerText = result.toFixed(2);
+    const result = calculateAmount(amountInPLN, currency);
+    updateResult(resultElement, result);
 });
 
 clearButton.addEventListener("click", () => {
-    amountInPLNElement.value = "";
-    resultElement.innerText = "N/A";
+    clearForm(amountInPLNElement, resultElement);
 });
+
