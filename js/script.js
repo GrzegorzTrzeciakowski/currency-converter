0e19a69ec1165a8839f21de58b1c6098795f741b
Grzegorz Trzeciakowski <gtrzeciakowski@gmail.com>
2024-03-20 20:04:43 +0100

    Updated JavaScript code structure and fixed reset button

--- js/script.js
+++ js/script.js
@@ -1,32 +1,32 @@
-const formElement = document.querySelector(".js-form");
-const amountInPLNElement = document.querySelector(".js-amount");
-const currencyElement = document.querySelector(".js-currency");
-const resultElement = document.querySelector(".js-result");
-const clearButton = document.querySelector(".js-clear");
+{
+    function calculateAmount(amount, rate) {
+        return amount * rate;
+    }
 
-function calculateAmount(amount, rate) {
-    return amount * rate;
+    function updateResult(amountInPLN, result) {
+        const resultElement = document.querySelector(".js-result");
+        resultElement.innerText = `${amountInPLN} EUR = ${result.toFixed(2)} PLN`;
+    }
+
+    function clearForm() {
+        const amountInPLNElement = document.querySelector(".js-amount");
+        const resultElement = document.querySelector(".js-result");
+        amountInPLNElement.value = "";
+        resultElement.innerText = "N/A";
+    }
+
+    const formElement = document.querySelector(".js-form");
+
+    formElement.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const amountInPLNElement = document.querySelector(".js-amount");
+        const currencyElement = document.querySelector(".js-currency");
+        const result = calculateAmount(parseFloat(amountInPLNElement.value), parseFloat(currencyElement.value));
+        updateResult(parseFloat(amountInPLNElement.value), result);
+    });
+
+    formElement.addEventListener("reset", (event) => {
+        clearForm();
+    });
 }
-
-function updateResult(resultElement, amountInPLN, currency, result) {
-    resultElement.innerText = `${amountInPLN} PLN = ${result.toFixed(2)} ${currency}`;
-}
-
-function clearForm(amountElement, resultElement) {
-    amountElement.value = "";
-    resultElement.innerText = "";
-}
-
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    const amountInPLN = parseFloat(amountInPLNElement.value);
-    const currency = parseFloat(currencyElement.value);
-
-    const result = calculateAmount(amountInPLN, currency);
-    updateResult(resultElement, amountInPLN, currencyElement.selectedOptions[0].text, result);
-});
-
-clearButton.addEventListener("click", () => {
-    clearForm(amountInPLNElement, resultElement);
-});
