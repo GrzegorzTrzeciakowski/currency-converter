4d81908e621e6a97c0eabccafceb9cf35538bcdd
Grzegorz Trzeciakowski <gtrzeciakowski@gmail.com>
2024-03-23 14:50:18 +0100

    Formated code with Prettier

--- js/script.js
+++ js/script.js
@@ -1,32 +1,35 @@
 {
-    function calculateAmount(amount, rate) {
-        return amount * rate;
-    }
+  function calculateAmount(amount, rate) {
+    return amount * rate;
+  }
 
-    function updateResult(amountInPLN, result) {
-        const resultElement = document.querySelector(".js-result");
-        resultElement.innerText = `${amountInPLN} EUR = ${result.toFixed(2)} PLN`;
-    }
+  function updateResult(amountInPLN, result) {
+    const resultElement = document.querySelector(".js-result");
+    resultElement.innerText = `${amountInPLN} EUR = ${result.toFixed(2)} PLN`;
+  }
 
-    function clearForm() {
-        const amountInPLNElement = document.querySelector(".js-amount");
-        const resultElement = document.querySelector(".js-result");
-        amountInPLNElement.value = "";
-        resultElement.innerText = "N/A";
-    }
+  function clearForm() {
+    const amountInPLNElement = document.querySelector(".js-amount");
+    const resultElement = document.querySelector(".js-result");
+    amountInPLNElement.value = "";
+    resultElement.innerText = "N/A";
+  }
 
-    const formElement = document.querySelector(".js-form");
+  const formElement = document.querySelector(".js-form");
 
-    formElement.addEventListener("submit", (event) => {
-        event.preventDefault();
+  formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-        const amountInPLNElement = document.querySelector(".js-amount");
-        const currencyElement = document.querySelector(".js-currency");
-        const result = calculateAmount(parseFloat(amountInPLNElement.value), parseFloat(currencyElement.value));
-        updateResult(parseFloat(amountInPLNElement.value), result);
-    });
+    const amountInPLNElement = document.querySelector(".js-amount");
+    const currencyElement = document.querySelector(".js-currency");
+    const result = calculateAmount(
+      parseFloat(amountInPLNElement.value),
+      parseFloat(currencyElement.value)
+    );
+    updateResult(parseFloat(amountInPLNElement.value), result);
+  });
 
-    formElement.addEventListener("reset", (event) => {
-        clearForm();
-    });
+  formElement.addEventListener("reset", (event) => {
+    clearForm();
+  });
 }
